b82e2b3f49639e65fe1b19676d155d51b20c6f82
Ron Galicia <dermalin3k@hotmail.com>
2015-10-21 15:52:15 -0700

    removed temp tables

--- comb.sql
+++ comb.sql
@@ -1,145 +1,24 @@
-#
-#### c
-#DROP TABLE IF EXISTS results_c;
-#CREATE TABLE results_c
-#AS
-#SELECT id AS c_id, name AS c_name, position AS c_position
-#FROM `selection test`
-#WHERE position = 'c';
-#
-#
-#### w
-#DROP table IF EXISTS table_w;
-#CREATE TABLE table_w
-#AS SELECT id,name,position
-#FROM `selection test`
-#WHERE position = 'w';
-#
-#DROP TABLE IF EXISTS results_w;
-#CREATE TABLE results_w
-#AS 
-#SELECT w1.id AS w1_id, w1.name AS w1_name, w1.position AS w1_position,
-#       w2.id AS w2_id, w2.name AS w2_name, w2.position AS w2_position,
-#       w3.id AS w3_id, w3.name AS w3_name, w3.position AS w3_position
-#FROM table_w AS w1, table_w AS w2, table_w AS w3
-#WHERE w1.id < w2.id
-#AND w2.id < w3.id;
-#
-#select c_id,w1_id,w2_id,w3_id
-#from results_c, results_w;
-#
-
-
-
-###ccc###
-DROP TABLE IF EXISTS results_c;
-CREATE TABLE results_c
-AS
-SELECT id AS c_id
-FROM `selection test`
-WHERE position = 'c';
-
-
-###www###
-DROP table IF EXISTS table_w;
-CREATE TABLE table_w
-AS SELECT id,name,position
-FROM `selection test`
-WHERE position = 'w';
-
-DROP TABLE IF EXISTS results_w;
-CREATE TABLE results_w
-AS 
-SELECT w1.id AS w1_id, w2.id AS w2_id, w3.id AS w3_id
-FROM table_w AS w1, table_w AS w2, table_w AS w3
-WHERE w1.id < w2.id
-AND w2.id < w3.id;
-
-select c_id,w1_id,w2_id,w3_id
-from results_c, results_w;
-
-
-
-
-
----------------------------------------------------
-
-DROP TABLE IF EXISTS Source;
-DROP TABLE IF EXISTS Numbers;
-DROP TABLE IF EXISTS Results;
-
-CREATE TABLE Source
- (
-   SourceId  int      not null AUTO_INCREMENT,
-   Element   char(1)  not null,
-   PRIMARY KEY(SourceId)
- );
-
-INSERT Source (Element) values ('A');
-INSERT Source (Element) values ('B');
-INSERT Source (Element) values ('C');
-INSERT Source (Element) values ('D');
-
-
-CREATE TABLE Numbers (Number int not null);
-INSERT Numbers (Number) values (1);
-INSERT Numbers (Number) values (2);
-INSERT Numbers (Number) values (3);
-INSERT Numbers (Number) values (4);
-INSERT Numbers (Number) values (5);
-INSERT Numbers (Number) values (6);
-INSERT Numbers (Number) values (7);
-INSERT Numbers (Number) values (8);
-INSERT Numbers (Number) values (9);
-INSERT Numbers (Number) values (10);
-
-
-CREATE TABLE Results
- (
-   Combo   varchar(10)  not null,
-   Length  int          not null
- );
-
------------------------------------------------------------
-
-
-
-###How many elements there are to process
-
-SELECT @MaxLoop := max(SourceId)
- from Source;
-
-
-###Initialize first value
-TRUNCATE TABLE Results;
-INSERT Results (Combo, Length)
- select Element, 1
-  from Source
-  where SourceId = 1;
-
-SET @Loop = 2;
-
-
-
-###Iterate to add each element after the first
-WHILE @Loop <= @MaxLoop
- BEGIN
-
-    INSERT Results (Combo, Length)
-     select distinct
-        left(re.Combo, @Loop - nm.Number)
-        + so.Element
-        + right(re.Combo, nm.Number - 1)
-       ,@Loop
-      from Results re
-       inner join Numbers nm
-        on nm.Number <= @Loop
-       inner join Source so
-        on so.SourceId = @Loop
-      where re.Length = @Loop - 1;
-
-
-    SET @Loop = @Loop + 1;
- END
-
-
+SELECT c1.id AS c1_id,
+       w1.id AS w1_id, w2.id AS w2_id, w3.id AS w3_id,
+       d1.id AS d1_id, d2.id AS d2_id,
+       g1.id AS g1_id,
+       util.id AS util_id
+FROM `selection test` AS c1,
+     `selection test` AS w1, `selection test` AS w2, `selection test` AS w3,
+     `selection test` AS d1, `selection test` AS d2,
+     `selection test` AS g1,
+     `selection test` AS util
+WHERE c1.position = 'c'
+AND w1.position = 'w' AND w2.position = 'w' AND w3.position = 'w'
+AND w1.id < w2.id
+AND w2.id < w3.id
+AND d1.position = 'd' AND d2.position = 'd'
+AND d1.id < d2.id
+AND g1.position = 'g'
+AND util.id != c1.id
+AND util.id != w1.id
+AND util.id != w2.id
+AND util.id != w3.id
+AND util.id != d1.id
+AND util.id != d2.id
+AND util.id != g1.id;
